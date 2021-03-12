14fe477929640e6e1ba8669dc777f79c6e7571d8
justlevine <justlevine@gmail.com>
2021-03-12 02:18:06 +0200

    Add GF factories

---
+++ tests/_support/Factories/Field.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace WPGraphQLGravityForms\Test\Factories;
+
+use GF_Fields;
+use WP_UnitTest_Generator_Sequence;
+
+class Field extends \WP_UnitTest_Factory_For_Thing {
+
+	public function __construct( $factory = null ) {
+		parent::__construct( $factory );
+		$this->default_generation_definitions = [
+			'id'    => new WP_UnitTest_Generator_Sequence( '%n' ),
+			'label' => new WP_UnitTest_Generator_Sequence( 'Field label %s' ),
+		];
+	}
+
+	public function create_object( $args ) {
+		$properties = [
+			'id'    => $args['id'] ?? null,
+			'type'  => $args['type'] ?? null,
+			'label' => $args['label'] ?? null,
+		];
+
+		$field = GF_Fields::create( $properties );
+
+		return $field;
+	}
+
+	public function create_many( $count, $args = [], $generation_definitions = null ) {
+		$fields = [];
+		for ( $n = 0; $n < $count; $n++ ) {
+			$field_args = $args;
+			$fields[]   = $this->create_object( $field_args );
+		}
+
+		return $fields;
+	}
+
+	public function update_object( $field, $args ) {
+		foreach ( $args as $key => $value ) {
+			$field->$key = $value;
+		}
+		return $field;
+	}
+
+	public function get_object_by_id( $field ) {
+		return $field;
+	}
+
+}
