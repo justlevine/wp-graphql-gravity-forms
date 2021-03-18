7188213d24e2691654f5ff64f61ca29e59ae442b
justlevine <justlevine@gmail.com>
2021-03-18 15:11:47 +0200

    fix!: replace SaveAndContinue `button` field with `buttonText`

--- src/Types/Form/SaveAndContinue.php
+++ src/Types/Form/SaveAndContinue.php
@@ -34,13 +34,16 @@ class SaveAndContinue implements Hookable, Type {
 			[
 				'description' => __( 'Gravity Forms form Save and Continue data.', 'wp-graphql-gravity-forms' ),
 				'fields'      => [
-					'enabled' => [
+					'enabled'    => [
 						'type'        => 'Boolean',
 						'description' => __( 'Whether the Save And Continue feature is enabled.', 'wp-graphql-gravity-forms' ),
 					],
-					'button'  => [
-						'type'        => Button::TYPE,
-						'description' => __( 'Contains the button text. Only applicable when type is set to text.', 'wp-graphql-gravity-forms' ),
+					'buttonText' => [
+						'type'        => 'string',
+						'description' => __( 'Contains the save button text.', 'wp-graphql-gravity-forms' ),
+						'resolve'     => function( $root ) : string {
+							return $root['button']['text'];
+						},
 					],
 				],
 			]
