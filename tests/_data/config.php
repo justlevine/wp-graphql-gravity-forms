de2f29842f55410aba9f3a1ed3bee2c71ea5f672
Dovid Levine <justlevine@gmail.com>
2022-03-09 20:38:11 +0000

    disable errors in config

--- tests/_data/config.php
+++ tests/_data/config.php
@@ -4,6 +4,6 @@
  * suite already bootstraps the autoloader and creates
  * fatal errors when the autoloader is loaded twice
  */
-define( 'GRAPHQL_DEBUG', true );
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
+// define( 'GRAPHQL_DEBUG', true );
+// define( 'WP_DEBUG', true );
+// define( 'WP_DEBUG_LOG', true );
