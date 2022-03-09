16580fd99d1e76219f0f984c4c33d72e528a91a8
Dovid Levine <justlevine@gmail.com>
2022-03-09 20:00:26 +0000

    fix rewrite structure

--- docker/app.post-setup.sh
+++ docker/app.post-setup.sh
@@ -5,8 +5,12 @@
 source _lib.sh
 
 install_plugins
-post_setup
 wp plugin activate wp-graphql-gravity-forms --allow-root
+
+# Set pretty permalinks.
+wp rewrite structure '/%year%/%monthnum%/%postname%/' --allow-root
+
+wp db export "${DATA_DUMP_DIR}/dump.sql" --allow-root
 
 # If maintenance mode is active, de-activate it
 if $( wp maintenance-mode is-active --allow-root ); then
