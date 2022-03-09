dc1a6f2227ebdd9a1f28d9211e5356f1c5884bee
Dovid Levine <justlevine@gmail.com>
2022-03-09 17:09:32 +0000

    attempt wpunit tests

---
+++ docker/app.post-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Activate wp-graphql
+source ../bin/_lib.sh
+
+install_gravityforms
+install_gravityforms_signature
+install_gravityforms_chainedselects
+install_gravityforms_quiz
+setup_plugin
+
+# If maintenance mode is active, de-activate it
+if $( wp maintenance-mode is-active --allow-root ); then
+  echo "Deactivating maintenance mode"
+  wp maintenance-mode deactivate --allow-root
+fi
