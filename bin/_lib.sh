e77098073e395790fe0976a1ba999dbddf2d159c
Dovid Levine <justlevine@gmail.com>
2022-03-10 01:04:17 +0000

    chore: dont forget to activate

--- bin/_lib.sh
+++ bin/_lib.sh
@@ -151,9 +151,6 @@ install_plugins() {
 	# Install WPJamstack Deployments and Activate
 	wp plugin install wp-jamstack-deployments --allow-root
 	wp plugin activate wp-jamstack-deployments --allow-root
-
-	# activate the plugin
-	wp plugin activate wp-graphql-gravity-forms --allow-root
 }
 
 setup_plugin() {
@@ -178,6 +175,10 @@ setup_plugin() {
 
 post_setup() {
 	cd $WP_CORE_DIR
+	
+	# activate the plugin
+	wp plugin activate wp-graphql-gravity-forms --allow-root
+
 	# Flush the permalinks
 	wp rewrite flush --allow-root
 
